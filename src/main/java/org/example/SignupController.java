688265b2431f762b0b2e3c2e79fc72bf3491557f
DamonEccl <damon.eccles@hotmail.com>
2024-06-30 00:34:57 +1000

    feat: password requirements

--- src/main/java/org/example/SignupController.java
+++ src/main/java/org/example/SignupController.java
@@ -38,14 +38,48 @@ public class SignupController {
         System.out.println(first + " " + confirmed);
 
         if (!first.isEmpty() && !confirmed.isEmpty()) {
-            if (first.equals(confirmed)) {
-                correct.setVisible(true);
-                Backend.setPassword(first);
-                passKeeperController.onPassKeeper();
-            } else {
+            int requirments = checkRequirements(first);
+            if (requirments == 0) {
+                if (first.equals(confirmed)) {
+                    correct.setVisible(true);
+                    Backend.setPassword(first);
+                    passKeeperController.onPassKeeper();
+                }
+                else {
+                    incorrect.setText("Passwords dont match");
+                    incorrect.setVisible(true);
+                }
+            }
+            else if (requirments == 1){
+                incorrect.setText("Password needs uppercase");
+                incorrect.setVisible(true);
+            }
+
+            else if (requirments == 2){
+                incorrect.setText("Password needs lowercase");
+                incorrect.setVisible(true);
+            }
+
+            else if (requirments == 3){
+                incorrect.setText("Password is not long enough");
                 incorrect.setVisible(true);
             }
         }
+    }
+
+    public int checkRequirements(String password){
+        if (password.equals(password.toLowerCase())){
+            return 1;
+        }
+
+        else if (password.equals(password.toUpperCase())){
+            return 2;
+        }
+
+        else if (password.length() < 8){
+            return 3;
+        }
+        return 0;
     }
 
 
